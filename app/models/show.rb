994e8abe378025a9d919d61f366e6c048b8a0d00
Miles Marillo <mjm03@hampshire.edu>
2019-05-02 13:12:02 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -17,7 +17,7 @@ class Show < ActiveRecord::Base
 	end
 
 	def self.least_popular_show
-		self.find(rating: self.lowest_rating)
+		self.find_by(rating: self.lowest_rating)
 		# returns the show with the lowest rating.
 	end
 
